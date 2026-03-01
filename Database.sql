dce10b0703fe53ae66b2769fb302f76356b934b4
NoSleepFox <SimenCodemaster@proton.me>
2026-03-01 15:30:50 +0100

    ferdig database

--- Database.sql
+++ Database.sql
@@ -1,28 +1,33 @@
 
 -- Mulig template for databasen
 
-create table medlem (
-    medlemsnummer,
-    fornavn,
-    etternavn,
-    telefonnummer,
-    epost,
-    postnummer/sted,
-    gatenavn/husnummer,
-    er_aktiv,
-    lag_id
+create table MEDLEM (
+    medlemsnummer INT PRIMARY KEY,
+    fornavn VARCHAR(50),
+    etternavn VARCHAR(50),
+    telefonnummer VARCHAR(20),
+    epost VARCHAR(100),
+    postnummer VARCHAR(4),
+    gatenavn VARCHAR(50),
+    er_aktiv BOOLEAN,
+    lag_id INT,
+    FOREIGN KEY (lag_id) references LOKALLAG(lag_id)
 );
 
-create table lokallag (
-    lag_id,
-    lag_navn,
-    leder,
-    postnummer/sted,
-    gatenavn/husnummer
+create table LOKALLAG (
+    lag_id INT PRIMARY KEY,
+    lagnavn VARCHAR(100),
+    leder_medlemsnummer INT,
+    postnummer VARCHAR(4),
+    gatenavn VARCHAR(50),
+    FOREIGN KEY (leder_medlemsnummer) references MEDLEM(medlemsnummer)
 );
 
-create table medlemsavgift (
-    medlemsnummer,
-    aar,
-    betalt
+create table BETALING (
+    medlemsnummer INT,
+    aar INT,
+    PRIMARY KEY (medlemsnummer, aar),
+    betalt BOOLEAN,
+    betalingsdato DATE,
+    FOREIGN KEY (medlemsnummer) references MEDLEM(medlemsnummer)
 );
